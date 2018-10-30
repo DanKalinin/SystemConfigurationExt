9131ac0bac3f1856fef1a0752bafda86aeb8f212
Dan Kalinin <daniil551@hotmail.com>
2018-10-30 15:13:59 +0300

    Project, podspec

---
+++ SystemConfigurationExt/SystemConfigurationExt.h
@@ -0,0 +1,19 @@
+//
+//  SystemConfigurationExt.h
+//  SystemConfigurationExt
+//
+//  Created by Dan Kalinin on 10/30/18.
+//  Copyright © 2018 Dan Kalinin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SystemConfigurationExt.
+FOUNDATION_EXPORT double SystemConfigurationExtVersionNumber;
+
+//! Project version string for SystemConfigurationExt.
+FOUNDATION_EXPORT const unsigned char SystemConfigurationExtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SystemConfigurationExt/PublicHeader.h>
+
+
